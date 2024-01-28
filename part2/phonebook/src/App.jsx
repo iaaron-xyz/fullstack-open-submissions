a30b5165888ee0dc4b071308a1243e4c9d279151
Aaron <iaaron.xyz@outlook.com>
2024-01-28 01:24:13 -0600

    2.6 step1 phonebook complete

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleInput = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmitName = (event) => {
+    // prevent reload page
+    event.preventDefault();
+    // add new person name
+    setPersons(
+      persons.concat({
+        name: newName,
+      })
+    );
+
+    // reset previous new name
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      {/* Add a new name */}
+      <form>
+        <div>
+          name: <input onChange={handleInput} value={newName} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmitName}>
+            add
+          </button>
+        </div>
+      </form>
+
+      {/* Display the list of person's name */}
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
