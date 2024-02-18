e623f64a16177bac8c306e34c799de0dfb60ef5c
Aaron <iaaron.xyz@outlook.com>
2024-02-18 02:27:11 -0600

    3.5 step5 PhonebookBackend complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -23,6 +25,29 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+// Get a random integer between 0 and top, top exclusive
+const getRandomInt = (top) => {
+  const topFloored = Math.floor(top);
+  return Math.floor(Math.random() * topFloored);
+};
+
+// get a random character from the chars string
+const getRandChar = () => {
+  return chars.charAt(getRandomInt(chars.length));
+};
+
+// generate a string with 20 random characters
+const genId = () => {
+  let id = "";
+  let idLength = 20;
+  for (let i = 0; i < 20; i += 1) {
+    id += getRandChar();
+  }
+  return id;
+};
 
 // Get phonebook info and date
 app.get("/info", (request, response) => {
@@ -79,6 +104,35 @@ app.delete("/api/persons/:id", (request, response) => {
   }
   // entry deleted
   response.status(204).end();
+});
+
+// Post a new phonebook entry
+app.post("/api/persons", (request, response) => {
+  // Get the post body
+  const body = request.body;
+  console.log(body);
+
+  // Deal with empty names
+  if (!body.name) {
+    return response.status(400).json({
+      error: "Missing name",
+    });
+  }
+
+  console.log("Random ID:", genId());
+
+  // Create new person object
+  const newPerson = {
+    id: genId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  // Add new person to phonebook list
+  persons = persons.concat(newPerson);
+
+  console.log("Persons list updated", persons);
+  response.json(newPerson);
 });
 
 const PORT = 3001;
