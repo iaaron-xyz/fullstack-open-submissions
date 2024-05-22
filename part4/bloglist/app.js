eea479986fc0b528664d2898b7188e5c5b261c71
Aaron <iaaron.xyz@outlook.com>
2024-05-22 01:59:44 -0600

    4.2 step2 Bloglist - complete

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const config = require("./utils/config");
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -6,16 +7,18 @@ const mongoose = require("mongoose");
 
 const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
-const config = require("./utils/config");
 
+// get the DB URI
 const mongoUrl = config.MONGODB_URI;
 
+// CONNECT to the DB
 mongoose.connect(mongoUrl);
 
-// Middlewares before routes
+// MIDDLEWARES before routes
 app.use(cors());
 app.use(express.json());
 
+// ROUTES
 app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,6 +1,5 @@
-const app = require("./app");
-
 const config = require("./utils/config");
+const app = require("./app");
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
